03fa0c5f8a8588b37f1b504fb15a65ac049dad0a
Ria Galanos <ria.galanos@gmail.com>
2018-10-13 23:05:01 -0400

    Initial commit

---
+++ src/main/java/com/cscheerleader/joopz/DashboardActivity.java
@@ -0,0 +1,13 @@
+package com.cscheerleader.joopz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.cscheerleader.joopz.R.layout.activity_dashboard);
+    }
+}
