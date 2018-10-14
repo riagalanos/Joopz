8ffc18bb016e53ac5ea6f230886ea296662b4776
Ria Galanos <ria.galanos@gmail.com>
2018-10-13 23:30:09 -0400

    Email login to Firebase working.

--- src/main/java/com/cscheerleader/joopz/LoginActivity.java
+++ src/main/java/com/cscheerleader/joopz/LoginActivity.java
@@ -21,7 +21,8 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
     private EditText input_email, input_password;
-    private Button button_create_account;
+    private Button button_create_account, button_login;
+    private String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         input_email = (EditText) findViewById(com.cscheerleader.joopz.R.id.input_email);
         input_password = (EditText) findViewById(com.cscheerleader.joopz.R.id.input_password);
         button_create_account = (Button) findViewById(com.cscheerleader.joopz.R.id.button_create_account);
+        button_login = (Button)findViewById(R.id.button_login);
         mAuth = FirebaseAuth.getInstance();
 
         // Hide ActionBar
@@ -38,12 +40,43 @@ public class LoginActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = input_email.getText().toString().trim();
+                password = input_password.getText().toString().trim();
+                //authenticate user
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if(!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        input_password.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+
         button_create_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String email = input_email.getText().toString().trim();
-                String password = input_password.getText().toString().trim();
+                email = input_email.getText().toString().trim();
+                password = input_password.getText().toString().trim();
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
@@ -60,7 +93,7 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
 
-                //create user
+                //create user and take to DashboardActivity
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
